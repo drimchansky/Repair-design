5a49218057a683777a5c8ea517f86f3e844b4759
DRIMchansky <drimchansky@gmail.com>
2019-09-15 06:54:33 +0300

    header sizing fixing

--- dist/main.d2070829.js
+++ dist/main.d2070829.js
@@ -263,7 +263,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "21695" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "23019" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
