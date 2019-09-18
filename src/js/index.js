69e57dbf0a8527cb2a0a66ed7d34bad4f1fe7604
DRIMchansky <drimchansky@gmail.com>
2019-09-18 04:15:44 +0300

    Swiper integrate

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
 import './scripts';
+
+import Swiper from 'swiper';
+
+var mySwiper = new Swiper('.swiper-container', {
+  autoplay: {
+    speed: 3000,
+    delay: 5000,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+});
