b5d52cc684fd3de979d2e328de9e18c3ae1b1364
DRIMchansky <drimchansky@gmail.com>
2019-09-30 08:42:26 +0300

    exhibit 1000

--- dist/js.00a46daa.js
+++ dist/js.00a46daa.js
@@ -10098,7 +10098,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "36568" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "1070" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
